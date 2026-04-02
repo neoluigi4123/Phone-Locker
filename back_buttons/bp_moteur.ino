6025e7bc264711e23bb689e3b6a576fa04b4caf5
Naël <142160688+Tokakuta@users.noreply.github.com>
2026-04-02 17:30:44 +0200

    ajout programme boutons

---
+++ back_buttons/bp_moteur.ino
@@ -0,0 +1,143 @@
+#include <Arduino_RouterBridge.h>
+#include <Wire.h>
+
+int current_pos = 1; // From 1 to 36
+
+#define MOTOR_ADDR 0x0F
+#define LED 13
+
+int lineFinderPin = 8;
+int bouton1 = 4;
+int bouton2 = 3;
+
+bool etat_porte = false;
+
+// motor commands
+#define SpeedSet     0x82
+#define DirectionSet 0xAA
+
+// directions
+#define M1_CW  0x01
+#define M1_CCW 0x02
+#define M2_CW  0x04
+#define M2_CCW 0x08
+
+void setSpeed(uint8_t m1, uint8_t m2) {
+    Wire.beginTransmission(MOTOR_ADDR);
+    Wire.write(SpeedSet);
+    Wire.write(m1);
+    Wire.write(m2);
+    Wire.endTransmission();
+    delay(50);
+}
+
+void setDirection(uint8_t dir) {
+    Wire.beginTransmission(MOTOR_ADDR);
+    Wire.write(DirectionSet);
+    Wire.write(dir);
+    Wire.write((uint8_t)0x00);
+    Wire.endTransmission();
+    delay(50);
+}
+
+// stop both motors
+void stop() {
+    setSpeed(0, 0);
+    setDirection(0x00);
+}
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  delay(2000);
+  Wire.begin();
+  Wire.setClock(50000);
+  delay(500);
+
+  // check connection
+  Wire.beginTransmission(MOTOR_ADDR);
+  if (Wire.endTransmission() != 0) {
+      while (1) {
+          digitalWrite(LED, HIGH); delay(80);
+          digitalWrite(LED, LOW);  delay(80);
+      }
+  }
+
+    stop();
+    delay(500);
+
+  pinMode(lineFinderPin, INPUT);
+
+  Serial.begin(115200);
+}
+
+void loop(){
+  if (digitalRead(bouton1) == 1){
+    if (etat_porte == 0){
+      ouvrir();
+    } else {
+      fermer();
+    }
+  }
+  while (digitalRead(bouton1) == 1){
+    delay(50);
+  }
+
+  if (digitalRead(bouton2) == 1)){
+    if (current_pos == 36){
+      current_pos = 1;
+    } else {
+      current_pos =+ 1;
+    }
+    Id(current_pos);
+    delay(1000);
+  }
+}
+
+void Move(int degrees, int percent) {
+  setDirection(M2_CW);
+  setSpeed(0, 255);
+  
+  bool prev_lineFinder = digitalRead(lineFinderPin);
+
+  while(digitalRead(lineFinderPin) == prev_lineFinder){
+    delay(50);
+  }
+  // Check when the lineFinderPin switches to black
+  while(digitalRead(lineFinderPin) == 0){
+    delay(50);
+  }
+      
+  current_pos += 1;
+  // stop
+  stop();
+  delay(300);
+}
+
+void Id(int id) {
+  int pos = map(id, 1, 36, 0, 350);
+  Move(pos, 60);
+  delay(1000);
+  ouvrir();
+  delay(1000);
+  fermer();
+}
+
+void ouvrir(){
+  setDirection(M1_CW);
+  setSpeed(128, 0);
+  delay(300);
+
+  // stop
+  stop();
+  delay(300);
+}
+
+void fermer() {
+  setDirection(M1_CCW);
+  setSpeed(128, 0);
+  delay(300);
+
+  // stop
+  stop();
+  delay(300); 
+}
